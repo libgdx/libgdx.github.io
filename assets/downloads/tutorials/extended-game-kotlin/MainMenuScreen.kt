f2ee14e450eb43ec35dd78cb7dc940454a7a53ab
iktools <75168625+iktools@users.noreply.github.com>
2021-11-25 15:26:20 +0100

    Init in property, render "with" batch

--- assets/downloads/tutorials/extended-game-kotlin/MainMenuScreen.kt
+++ assets/downloads/tutorials/extended-game-kotlin/MainMenuScreen.kt
@@ -5,24 +5,24 @@ import com.badlogic.gdx.Screen
 import com.badlogic.gdx.graphics.OrthographicCamera
 import com.badlogic.gdx.utils.ScreenUtils
 
-class MainMenuScreen(private val game: Drop):Screen {
+class MainMenuScreen(private val game: Drop): Screen {
 
-    private var camera: OrthographicCamera = OrthographicCamera()
-
-    init {
-        camera.setToOrtho(false, 800f, 480f)
+    private var camera = OrthographicCamera().apply {
+        setToOrtho(false, 800f, 480f)
     }
 
     override fun render(delta: Float) {
         ScreenUtils.clear(0f, 0f, 0.2f, 1f);
 
         camera.update();
-        game.batch.projectionMatrix = camera.combined;
+        with (game.batch) {
+            projectionMatrix = camera.combined;
 
-        game.batch.begin();
-        game.font.draw(game.batch, "Welcome to Drop!!! ", 100f, 150f);
-        game.font.draw(game.batch, "Tap anywhere to begin!", 100f, 100f);
-        game.batch.end();
+            begin();
+            game.font.draw(this, "Welcome to Drop!!! ", 100f, 150f);
+            game.font.draw(this, "Tap anywhere to begin!", 100f, 100f);
+            end();
+        }
 
         if (Gdx.input.isTouched) {
             game.screen = GameScreen(game);
